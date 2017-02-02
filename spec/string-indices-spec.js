a2005a143d6ce048dddc093ef67edd8104a6166f
Monica Williams <monicaestellaw@gmail.com>
2017-02-02 14:20:04 -0800

    made changes per di review

--- spec/string-indices-spec.js
+++ spec/string-indices-spec.js
@@ -8,8 +8,11 @@ describe( 'stringIndices', () => {
   it( 'exists', () => {
     expect( stringIndices ).to.be.a( 'function' )
   })
-  it('returns an empty string if the supplied index is longer or shorter than the length of the string.', () => {
+  it('returns an empty string if the supplied index is greater than the length of the string.', () => {
     expect( stringIndices('It is such a lovely day.', -3)).to.equal( '' )
+  })
+  it('returns an empty string if the supplied index is less than the length of the string.', () => {
+    expect( stringIndices('It is truly a lovely day.', 0)).to.equal( '' )
   })
   it('returns the word at the specified index.', () => {
     expect( stringIndices('I mean really a gorgeous day.', 2)).to.equal( 'mean' )
