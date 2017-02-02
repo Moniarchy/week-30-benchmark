63afee2d3e1dbb4f3fb50cc58fbf6619b472c4ef
Monica Williams <monicaestellaw@gmail.com>
2017-02-02 09:07:33 -0800

    switching to new branch

--- spec/scrab-bag-test.js
+++ spec/scrab-bag-test.js
@@ -1,2 +1,17 @@
 const chai = require( 'chai' )
 const { expect } = require( 'chai' )
+
+//record how many of each letter there are in the bag
+//cut the string into individual strings in an array
+//iterate through the string and then find those values in the second, larger array. splice them
+//add the amount of characters there are left of a certain value in the larger array
+//if else or switch statement to print the numbers
+
+//OR
+
+//record how many of each letter there are in the bag
+//cut the string into individual strings in an array
+//count how many of each character are found in the shorter array
+//subtract that from how many of each letter there were in the bag
+
+//match up the number/letter pairs and console.log them
--- src/scrab-bag.js
+++ src/scrab-bag.js
@@ -1,40 +1,48 @@
-### 2: Scrab-Bag
 
-Scrabble (the game in which players remove tiles with letters on them from a bag and use them to create words on the game board) has a set number of 100 tiles.
 
-This number does not change between games, nor does the frequency of each letter.
+const scrabBag = tiles => {
+  if ( typeof tiles !== 'string' ) {
+  console.log( 'Help! I only work with a string!' )
+} else {
+  const bag = [
+    'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'A', 'A', 'A',
+    'A', 'A', 'A', 'A', 'A', 'A', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+    'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'N', 'N', 'N', 'N', 'N', 'N', 'R',
+    'R', 'R', 'R', 'R', 'R', 'T', 'T', 'T', 'T', 'T', 'T', 'L', 'L', 'L', 'L',
+    'S', 'S', 'S', 'S', 'U', 'U', 'U', 'U', 'D', 'D', 'D', 'D', 'G', 'G', 'G',
+    '_', '_', 'B', 'B', 'C', 'C', 'M', 'M', 'P', 'P', 'F', 'F', 'H', 'H', 'V',
+    'V', 'W', 'W', 'Y', 'Y', 'K', 'J', 'X', 'Q', 'Z'
+  ]
+  const playedPieces = tiles.split('')
 
-[Frequency and point value of each tile.](http://scrabblewizard.com/scrabble-tile-distribution/)
+  for ( index = 0; index < playedPieces.length; index++ ) {
+    bag.find()
+    while ( bag[jindex] !== playedPieces[index] ) { //loop through second array while we have not found an instance of the value
+      if ( playedPieces[index] === bag[index] ) {
+        bag.splice( bag[jindex], 1)
+        }
+      }
+    } console.log(bag)
+  }
+}
 
-Represent each tile by the letter that appears on it, and blank tiles by underscores (`_`).
 
-**Input:**
+//now i have two arrays. i need to loop through the first array and record the value.
+//then i need to loop through the second array and search the whole thing and take out the first instance of the value
+//after all that, i have my updated array and can proceed
 
-An uppercase string with the tiles that have already been played.
+scrabBag('E')
+//record how many of each letter there are in the bag
+//cut the string into individual strings in an array
+//iterate through the string and then find those values in the second, larger array. splice them
+//add the amount of characters there are left of a certain value in the larger array
+//if else or switch statement to print the numbers
 
-E.g., if 14 tiles have been played, you would be given an input like this:
-
-```
-AERETOXMYCNS_B
-```
-
-**Output:**
-
-```
-10: E
-9: I
-8: A
-7: O
-5: R, N, T
-4: U, L, D
-3: G, S
-2: P, H, F, V, W
-1: B, Y, J, K, M, Q, C, Z, _
-0: X
-```
+//OR
 
 //record how many of each letter there are in the bag
-//parse the string, perhaps iterating through it?
-//count how many of each character are found in the string
+//cut the string into individual strings in an array
+//count how many of each character are found in the shorter array
 //subtract that from how many of each letter there were in the bag
+
 //match up the number/letter pairs and console.log them
